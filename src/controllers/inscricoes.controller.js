e830ae22f900458c94384826e269c10790e28b8d
Ezer Mello <ezer.mello@prof.infnet.edu.br>
2022-08-17 21:36:45 -0300

    feat: wip validate dto with joi

--- src/controllers/inscricoes.controller.js
+++ src/controllers/inscricoes.controller.js
@@ -1,20 +1,65 @@
 const incluiInscricaoMapper = require('../mapper/inscricoes/inclui-inscricao.mapper');
 const excluiInscricaoMapper = require('../mapper/inscricoes/inclui-inscricao.mapper');
+const joi = require('joi');
+
 
 const incluiInscricao = (req, res) => { 
     
     //todo: extrair dados
     const { params, body } = req;
 
+    console.log(params)
+    const paramSchema = joi.object({
+        id: joi.number().required(),
+    });    
+    
+    const paramResult = paramSchema.validate(params);
+    if (paramResult.error) {
+
+        const { details } = paramResult.error;                
+
+        const detailsMessages = details.reduce((acc, item) => {
+
+            return [ ...acc, item.message ]
+        
+        }, [])
+
+        return res.status(400).json({
+            mensagem: detailsMessages.join(';')
+        });                        
+    }
+    
+    
     //todo: validar os dados     
+    if(!params.id || params.id == ' ') {
+        
+        return res.status(400).json({
+            mensagem: 'curso não informado'
+        });
+    } 
+
+    if (
+        (!body.alunoNome) || (body.alunoNome.length < 5)
+    ) {
+
+        return res.status(400).json({
+            mensagem: 'o nome do aluno esta incorreto'
+        });
+
+    }
+
+
 
 
     //todo: transformando dados da entra em objetos de negocio FALTOU
+
+
     //todo: camada de negocio
     
     //todo: montar objeto de saida 
     res.json(incluiInscricaoMapper.domainToDto({})); 
 
+    
 }
 
 const excluiInscricao = (req, res) => {
